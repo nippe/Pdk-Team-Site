b83fadc393973079828eae832dc932434de5de96
Niklas Nihlen <niklas.nihlen@gmail.com>
2011-03-02 14:41:27 +0100

    Clean project, initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PdkTeamSite::Application.config.secret_token = '4ea16e58f31044d79dbc6cb5b72acab9522066260e39ce1e45b9715a2b8e0525cc1da7f962b1a7802f7a6ddcb57d267dc9eb89c57cee2480481e0834e85538c2'
