dfeda464dba94c3863f078b00dfcc1ce0ca3fe0a
Niklas Nihlen <niklas.nihlen@gmail.com>
2011-03-09 00:12:05 +0100

    Got contact with jQuery on the players page

---
+++ public/javascripts/players.js
@@ -0,0 +1,13 @@
+/**
+ * Created by .
+ * User: niklasni
+ * Date: 2011-03-09
+ * Time: 00:04
+ * To change this template use File | Settings | File Templates.
+ */
+
+$(document).ready(
+        function(){
+            alert("FOFOFOFOF");
+        }
+ );
