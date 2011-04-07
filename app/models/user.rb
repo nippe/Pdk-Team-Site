695075c3e0d795c216c248db8eb07f8a11f7559c
Niklas Nihlen <niklas.nihlen@gmail.com>
2011-04-07 22:30:36 +0200

    First small attempt at securing the new user form from spambots

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,12 @@ class User < ActiveRecord::Base
   acts_as_authentic
   
   has_many :rvsp
+  validates_uniqueness_of :login
+  validates_uniqueness_of :email
+
+  # My own spam filtering
+  attr_accessor :noshow # fake attribute used for spam trapping
+  validates_length_of :noshow, :in => 0..1
 
   def full_name
     first_name + " " + last_name
