508799c5dfeb07294aab759e9398345c124a210f
Niklas Nihlen <niklas.nihlen@gmail.com>
2011-06-16 23:07:32 +0200

    Changed first day of week from Sunday to Monday

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -1,12 +1,15 @@
 class CalendarController < ApplicationController
   
   def index
+    #weeks starts with monday in sweden
+    @first_day_of_week = 1
+
     @month = (params[:month] || (Time.zone || Time).now.month).to_i
     @year = (params[:year] || (Time.zone || Time).now.year).to_i
 
     @shown_month = Date.civil(@year, @month)
 
-    @event_strips = Event.event_strips_for_month(@shown_month)
+    @event_strips = Event.event_strips_for_month(@shown_month, @first_day_of_week)
   end
   
 end
--- app/helpers/calendar_helper.rb
+++ app/helpers/calendar_helper.rb
@@ -12,7 +12,8 @@ module CalendarHelper
       :month_name_text => I18n.localize(@shown_month, :format => "%B %Y"),
       :previous_month_text => "<< " + month_link(@shown_month.prev_month),
       :next_month_text => month_link(@shown_month.next_month) + " >>",
-      :use_all_day => true
+      :use_all_day => true,
+      :first_day_of_week => @first_day_of_week
     }
   end
 
