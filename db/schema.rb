23d58ce8597c95558cebb85bf56afc9e18d32176
Niklas Nihlen <niklas.nihlen@gmail.com>
2011-06-16 23:03:44 +0200

    Runed db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110414191841) do
+ActiveRecord::Schema.define(:version => 20110616205704) do
 
   create_table "activities", :force => true do |t|
     t.string   "title"
@@ -25,6 +25,15 @@ ActiveRecord::Schema.define(:version => 20110414191841) do
 
   create_table "activity_types", :force => true do |t|
     t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "events", :force => true do |t|
+    t.string   "name"
+    t.datetime "start_at"
+    t.datetime "end_at"
+    t.boolean  "all_day",    :default => false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
