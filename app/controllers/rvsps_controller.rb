07eea0e4e4b6b8186d220bf673694be178cf8f85
Niklas Nihlen <niklas.nihlen@gmail.com>
2011-07-06 00:53:27 +0200

    Forgot one little change

--- app/controllers/rvsps_controller.rb
+++ app/controllers/rvsps_controller.rb
@@ -5,14 +5,20 @@ class RvspsController < ApplicationController
     @rvsps = Rvsp.find_by_activity_id(params[:activity_id])
   end
 
+  #TODO: Make private
+  def generate_json(activity)
+    "{\"result\":\"ok\", \"user_id\":" + params[:user_id] + ", \"logged_in_user_id\":" + current_user.id.to_s + ",\n\"activity_summary\":{\"activity_id\": " + params[:activity_id] + ",\"attending\":" + activity.attending.count.to_s + ", \"maybe\":" + activity.maybe_coming.count.to_s + ",\"not_comming\":" + activity.not_coming.count.to_s + ", \"no_answer\":" + activity.no_answer.count.to_s + "}}"
+  end
 
+#TODO: Maybe use current_user here in the controller instead for posting it via the url, that way you have to be logged on to rvsp...
   def attend
     @rvsp = Rvsp.find_by_activity_id_and_user_id(params[:activity_id], params[:user_id])
     @rvsp.rvsp_status_id = 1
+    activity = Activity.find_by_id(params[:activity_id])
 
     respond_to do |format|
       if @rvsp.save
-        format.json { render :json => "[{\"result\":\"ok\"}]" }
+        format.json { render :json => generate_json(activity) }
         format.html { redirect_to(:activities, :notice => "Du er anmld till trningen") }
         format.xml { render :xml => @rvsp, :status => :attending, :location => :activities }
       else
@@ -28,8 +34,8 @@ class RvspsController < ApplicationController
 
     respond_to do |format|
       if rvsp.save
-        format.json{ render :json => rvsp.activity, :json => rvsp.activity.rvsps }  #TODO: Fix so this returns a summary of rvsps for the activity
-        format.html{ redirect_to(activities_path, :notice => "Du er anmld till trningen")}
+        format.json { render :json => rvsp.activity, :json => rvsp.activity.rvsps } #TODO: Fix so this returns a summary of rvsps for the activity
+        format.html { redirect_to(activities_path, :notice => "Du er anmld till trningen") }
       end
     end
   end
--- public/javascripts/activities-ui.js
+++ public/javascripts/activities-ui.js
@@ -26,21 +26,53 @@ $(document).ready(
         );
 
 
-
 $(document).ready(
-    function() {
-        $(".rvsp-dropdown").change(
-            function() {
-                if($(this).val() == 1) {
-                    $(this).removeClass('background-yellow').removeClass('background-red').removeClass('background-grey').addClass('background-green');
-                }
-                else if($(this).val() == 2){
-                    $(this).removeClass('background-green').removeClass('background-red').removeClass('background-grey').addClass('background-yellow');
-                }
-                else{
-                    $(this).removeClass('background-green').removeClass('background-yellow').removeClass('background-grey').addClass('background-red');
-                }
-            }
+        function() {
+            var action;
+            $(".rvsp-dropdown").change(
+                    function() {
+                        rvspElementRef = $(this);
+
+                        //alert($(this).parent().parent().find('.nav-green-bar').eq(0).attr('height', '20'));
+
+                        if ($(this).val() == 1) {
+                            $(this).removeClass('background-yellow').removeClass('background-red').removeClass('background-grey').addClass('background-green');
+                            action = 'attend';
+                        }
+                        else if ($(this).val() == 2) {
+                            $(this).removeClass('background-green').removeClass('background-red').removeClass('background-grey').addClass('background-yellow');
+                            action = 'maybe';
+                        }
+                        else {
+                            $(this).removeClass('background-green').removeClass('background-yellow').removeClass('background-grey').addClass('background-red');
+                            action = 'not';
+                        }
+
+                        var str = '/activities/';
+                        var activityId = $(this).attr('id').split('-').pop();
+
+                        var url = '/activities/' + activityId + '/rvsps/' + action + '/' + userId + '.json';
+                        //alert(url);
+                        $.getJSON(
+                                url,
+                                function(json) {
+                                    var barMaxHeight = 14;
+                                    var totalCount = json.activity_summary.attending + json.activity_summary.maybe + json.activity_summary.not_comming + json.activity_summary.no_answer;
+                                    var greenHeight = Math.round(((json.activity_summary.attending / totalCount) * barMaxHeight));
+                                    rvspElementRef.parent().parent().find('.nav-green-bar').eq(0).attr('height', greenHeight);
+
+                                    var yellowHeight = Math.round((json.activity_summary.maybe / totalCount) * barMaxHeight);
+                                    rvspElementRef.parent().parent().find('.nav-yellow-bar').eq(0).attr('height', yellowHeight);
+
+                                    var redHeight = Math.round((json.activity_summary.not_comming / totalCount) * barMaxHeight);
+                                    rvspElementRef.parent().parent().find('.nav-red-bar').eq(0).attr('height', redHeight);
+
+                                    var greyHeight = Math.round((json.activity_summary.no_answer / totalCount) * barMaxHeight);
+                                    rvspElementRef.parent().parent().find('.nav-grey-bar').eq(0).attr('height', greyHeight);
+
+                                }
+                                );
+                    }
+                    );
+        }
         );
-    }
-);
