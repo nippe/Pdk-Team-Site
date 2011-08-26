d53b665d2b68fa2c061096ebbe01f6198839b179
Niklas Nihlen <niklas.nihlen@gmail.com>
2011-08-26 18:38:01 +0200

    Changed role names in seed file to be lower case

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,9 +12,9 @@ ActivityType.create(:name => 'Match', :image_path => '/images/indicators/activit
 ActivityType.create(:name => 'Annan aktivitet', :image_path => '/images/indicators/calendar_month.png')
 
 Role.delete_all
-Role.create(:name => 'Admin')
-Role.create(:name => 'Player')
-Role.create(:name => 'Guest')
+Role.create(:name => 'admin')
+Role.create(:name => 'player')
+Role.create(:name => 'guest')
 
 Activity.delete_all
 Activity.create(:title => 'Fake activity', :start_at => Time.now + 1.month, :all_day => true, :activity_type_id => 1)
