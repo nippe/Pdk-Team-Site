0edf79762bbd2800202cf15ac7fd5e5335644925
Niklas Nihlen <niklas.nihlen@gmail.com>
2011-09-23 09:16:25 +0200

    Stepped back and removed the auto complete stuff that I didn't get to work. Going with a simple dropdown first and push the auto complete goodness forward.
    Crated and hooked up jQuery script for the expense form. Not doing anything of value yet.

---
+++ public/javascripts/expenses.js
@@ -0,0 +1,21 @@
+/**
+ * Created by JetBrains RubyMine.
+ * User: niklasni
+ * Date: 2011-09-23
+ * Time: 09:07
+ * To change this template use File | Settings | File Templates.
+ */
+
+
+$(document).ready(
+
+        function(){
+            $('#add-to-shit-list').click(
+                    function(){
+                       //TODO: Add user to debt-list
+
+                    }
+            )
+        }
+
+);
